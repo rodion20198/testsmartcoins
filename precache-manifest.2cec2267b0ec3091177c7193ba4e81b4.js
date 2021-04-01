7fee8a729ec967112de44aadacf3ac88b46eb088
rodion20198 <53440858+rodion20198@users.noreply.github.com>
2021-04-02 02:57:27 +0700

    Updates

---
+++ precache-manifest.2cec2267b0ec3091177c7193ba4e81b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa68ef510dfde188f490",
+    "url": "./static/css/main.7d23b95d.chunk.css"
+  },
+  {
+    "revision": "fa68ef510dfde188f490",
+    "url": "./static/js/main.554f4f39.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "383623808debd2e7b7ec",
+    "url": "./static/css/2.08c5fad3.chunk.css"
+  },
+  {
+    "revision": "383623808debd2e7b7ec",
+    "url": "./static/js/2.de5f65d4.chunk.js"
+  },
+  {
+    "revision": "60410541ac885d6bc59ad3ade70ad369",
+    "url": "./index.html"
+  }
+];
